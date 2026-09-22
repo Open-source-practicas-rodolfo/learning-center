48fd61577e4887af00d0462dead64b4a65e08983
Rodolxde <gutierrezrodolfo360@gmail.com>
2026-09-22 12:09:59 -0500

    feat(page-not-found): add page Not Found view with translation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,12 @@ import { About } from './shared/presentation/views/about/about';
 const baseTitle ='ACME Learning Center'
 
 const about = () =>import('./shared/presentation/views/about/about').then(m => m.About);
+const pageNotFound = () =>
+  import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
 export const routes: Routes = [
-{ path: 'home', component: Home, title: `${baseTitle} - home`},
-  {path: 'about', loadComponent: about, title: `${baseTitle} - About`},
+  { path: 'home',     component: Home, title: `${baseTitle} - home`},
+  { path: 'about',    loadComponent: about, title: `${baseTitle} - About`},
+  { path: '',         redirectTo: '/home', pathMatch: 'full' },
+  { path: '**',       loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found`},
 
 ];
