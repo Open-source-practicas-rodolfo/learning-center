4f09e7370c247908d0fa404510211dbff5d243ea
Rodolxde <gutierrezrodolfo360@gmail.com>
2026-09-22 11:15:03 -0500

    feat(home): add Home view with title and content including route definition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './shared/presentation/views/home/home';
 
-export const routes: Routes = [];
+const baseTitle ='ACME Learning Center'
+
+
+export const routes: Routes = [
+{path: 'home', component: Home, title: `${baseTitle} - home`}
+
+];
